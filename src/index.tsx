e4637a0645c4ce016ef38f134f69c8f6f673c42c
최유진 <yjyj0774@gmail.com>
2024-07-19 13:07:04 +0900

    useContext 함수 생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./useEffect";
+import App from "./useContext";
+// import App from "./useEffect";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
