2eeaa69d73ce61e7757766d28abed3657846e84c
최유진 <yjyj0774@gmail.com>
2024-07-19 16:33:16 +0900

    context 생성

---
+++ src/themeProvider.tsx
@@ -0,0 +1,20 @@
+import React, { createContext, useState, ReactNode } from "react";
+
+// 테마 정보를 담을 Context 상자 만들기
+const ThemeContext = createContext();
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+const ThemeProvider = ({ children }) => {
+  // 기본 테마를 'light'로 설정
+  const [theme, setTheme] = useState("light");
+
+  // 상자의 value에 정보를 넣어서 컴포넌트 트리의 하위 컴포넌트들이 사용할 수 있도록 한다.
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
