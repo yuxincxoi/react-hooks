ea5facaeaf3c4b643aca83081ddcac45bcea931b
최유진 <yjyj0774@gmail.com>
2024-07-20 20:19:39 +0900

    useContext 사용하여 ThemeContext에서 theme, setTheme 가져오기

--- src/themeComponent.tsx
+++ src/themeComponent.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "./themeProvider";
 
-const ThemeComponent = () => {};
+const ThemeComponent = () => {
+  // useContext를 사용하여 ThemeContext에서 theme과 setTheme 가져오기
+  const { theme, setTheme } = useContext(ThemeContext);
+};
 
 export default ThemeComponent;
