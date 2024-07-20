189ef039436f3a90dc8b99a31c0b53a2c1e42887
최유진 <yjyj0774@gmail.com>
2024-07-20 20:23:46 +0900

    ThemedComponent를 ThemeProvider로 감싸서 테마 정보를 전달

--- src/useContext.tsx
+++ src/useContext.tsx
@@ -1,7 +1,14 @@
 import React, { useContext, useState } from "react";
+import { ThemeProvider } from "./themeProvider";
+import ThemeComponent from "./themeComponent";
 
 const App = () => {
-  return <div></div>;
+  // ThemedComponent를 ThemeProvider로 감싸서 테마 정보를 전달
+  return (
+    <ThemeProvider>
+      <ThemeComponent />
+    </ThemeProvider>
+  );
 };
 
 export default App;
