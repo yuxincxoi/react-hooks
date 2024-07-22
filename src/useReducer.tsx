8272fe03cb636bfe7b8da3d84907b07e83151d4e
최유진 <yjyj0774@gmail.com>
2024-07-22 13:01:24 +0900

    state 타입 정의

--- src/useReducer.tsx
+++ src/useReducer.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 
-// count 초기값 설정
-const initialCount = { count: 0 };
+// 상태와 액션 타입 정의
+interface State {
+  count: number;
+  text: string;
+}
 
-const reducer = (state, action) => {
+// count 초기값 설정
+const initialCount: State = { count: 0, text: "" };
+
+const reducer = (state: State, action): State => {
   switch (action.type) {
     case "INCREMENT":
-      return { count: state.count + 1 };
+      return { ...state, count: state.count + 1 }; // count 속성만 업데이트하고, 나머지 state 속성은 그대로 유지
     case "DECREMENT":
-      return { count: state.count - 1 };
+      return { ...state, count: state.count - 1 };
     default:
       throw new Error();
   }
