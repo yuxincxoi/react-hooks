17c931c3156524f96ba1a4c84dae89d76053cfe9
최유진 <yjyj0774@gmail.com>
2024-07-22 12:55:35 +0900

    reducer 함수 정의

--- src/useReducer.tsx
+++ src/useReducer.tsx
@@ -1,3 +1,15 @@
 import React from "react";
 
+// count 초기값 설정
 const initialCount = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
